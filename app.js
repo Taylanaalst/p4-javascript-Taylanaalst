d0068c48210ed9e581dc860d5a9bf7207758b22f
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2022-05-16 15:02:08 +0200

    opdr 1-2

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 let name;
 let country;
-let p = document.getElementById("output");
+// let p = document.getElementById("output");
 
-const nameChanged = function(event){
-    name = event.target.value;
-    changeInput();
-    }
+// const nameChanged = function(event){
+//     name = event.target.value;
+//     changeInput();
+//     }
 
-const countryChange = function(event){
-    country = event.target.value;
-    changeInput();
-}
+// const countryChange = function(event){
+//     country = event.target.value;
+//     changeInput();
+// }
 
 const changeInput = function(){
     const text = `Je heet ${name} en je komt uit ${country}`;
     p.textContent = text;
 }
 
+nameChanged.addEventlistener("change", function()){
+    name= event.target.value;
+    changeInput();
+}
 
+countryChanged.addEventlistener("change", function(){
+    country = event.target.value;
+    changeInput();
+})
+
+const changeInput = function(){
+    const text = `Je heet ${name} en je komt uit ${country}`;
+    p.textContent = text;
+}
