90e296a71590ae264f94909307baf54d2b2eb078
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2022-05-20 12:10:06 +0200

    opdracht 2

--- les2/app.js
+++ les2/app.js
@@ -9,3 +9,31 @@ bird.addEventListener("click", function(){
 })
 
 
+window.addEventListener("keydown", function(event){
+    if(event.key == "ArrowRight"){
+        count += 50;
+        bird.style.left = count + "px"
+    }
+})
+
+
+window.addEventListener("keydown", function(event){
+    if(event.key == "ArrowLeft"){
+        count -= 50;
+        bird.style.left = count + "px"
+    }
+})
+
+window.addEventListener("keydown", function(event){
+    if(event.key == "ArrowUp"){
+        count += 50;
+        bird.style.top = count + "px"
+    }
+})
+
+window.addEventListener("keydown", function(event){
+    if(event.key == "ArrowDown"){
+        count += 50;
+        bird.style.bottom = count + "px"
+    }
+})
