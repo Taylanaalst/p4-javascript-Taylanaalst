ca839510f861ae7be72b8fd33a49094f7cfa4d6c
Taylanaalst <90317611+Taylanaalst@users.noreply.github.com>
2022-05-30 14:07:06 +0200

    oefening 3 - Change the moving bird

--- les2/app.js
+++ les2/app.js
@@ -13,6 +13,7 @@ window.addEventListener("keydown", function(event){
     if(event.key == "ArrowRight"){
         count += 50;
         bird.style.left = count + "px"
+        bird.src = "img/b-right.svg"
     }
 })
 
@@ -21,13 +22,15 @@ window.addEventListener("keydown", function(event){
     if(event.key == "ArrowLeft"){
         count -= 50;
         bird.style.left = count + "px"
+        bird.src = "img/b-left.svg"
     }
 })
 
 window.addEventListener("keydown", function(event){
     if(event.key == "ArrowUp"){
         count += 50;
-        bird.style.top = count + "px"
+        bird.style.top = count - "px"
+        bird.src = "img/b-up.svg"
     }
 })
 
@@ -35,5 +38,7 @@ window.addEventListener("keydown", function(event){
     if(event.key == "ArrowDown"){
         count += 50;
         bird.style.bottom = count + "px"
+        bird.src = "img/b-down.svg"
     }
 })
+
